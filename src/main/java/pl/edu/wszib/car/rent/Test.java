b59ff8b079d2348313e4020163ca014a9d905e71
Mateusz <bmatteo@interia.pl>
2023-11-10 12:46:05 +0100

    update2

--- src/main/java/pl/edu/wszib/car/rent/Test.java
+++ src/main/java/pl/edu/wszib/car/rent/Test.java
@@ -3,5 +3,7 @@ package pl.edu.wszib.car.rent;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Pierwszy commit");
+
+        System.out.println("Jakas zmiana");
     }
 }
